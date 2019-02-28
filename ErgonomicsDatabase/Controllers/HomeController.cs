7726baa27e0e0694de41a4625389a56a19574220
Alexis <aat0022@auburn.edu>
2019-02-28 14:03:42 -0600

    Added New Entry page

--- ErgonomicsDatabase/Controllers/HomeController.cs
+++ ErgonomicsDatabase/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace ErgonomicsDatabase.Controllers
             return View();
         }
 
+        public IActionResult NewEntry()
+        {
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
