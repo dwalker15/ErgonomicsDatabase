abf191b76589e0ddeff2a805ba3adebf1c23268c
Sarah Till <slt0025@auburn.edu>
2019-03-28 14:13:31 -0500

    view

---
+++ ErgonomicsDatabase/Models/ViewModels/TorqueTableViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ErgonomicsDatabase.Models.ViewModels
+{
+    public class TorqueTableViewModel
+    {
+        public string ObjectHandle { get; set; }
+        public string RotationAxis { get; set; }
+        public string Posture { get; set; }
+        public string Hand { get; set; }
+        public int FemaleCount { get; set; }
+        public int MaleCount { get; set; }
+        public int BeginAgeRange { get; set; }
+        public int EndAgeRange { get; set; }
+        public int MeanFemale { get; set; }
+        public int SDFemale { get; set; }
+        public int MeanMale { get; set; }
+        public int SDMale { get; set; }
+        public string Source { get; set; }
+        
+        //public TorqueTableViewModel()
+        //{
+        //}
+    }
+}
